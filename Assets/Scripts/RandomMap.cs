23bd161cb3ebb41ad0424d64e53276d761d03d6a
INB <inbal.lavi1@mail.huji.ac.il>
2022-02-01 02:10:47 +0200

    random map fix

--- Assets/Scripts/RandomMap.cs
+++ Assets/Scripts/RandomMap.cs
@@ -1,18 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class RandomMap : MonoBehaviour
 {
     // [SerializeField] private GameObject[] objectsToSpread;
-    private String[] objectsToSpreadString = new []{"Stop", "Stop", "Stop", "Stop", "Stop",  "Stop", "Stop", "Stop", "Stop", "Stop", "Speed","Time", "Bomb"};
+    private List<String> objectsToSpreadString = new List<string>
+    {
+        "Speed", "Speed", "Speed", "Speed",
+        "Bomb", "Bomb", "Bomb",
+        "Time", "Time"
+    };
 
     void Start()
     {
-        int i = Random.Range(0, objectsToSpreadString.Length);
-        GameObject itemToSpreadLoad = (GameObject) Resources.Load(objectsToSpreadString[i], typeof(GameObject));
-        Instantiate(itemToSpreadLoad, transform.position, Quaternion.identity);
+        int positionsCount = transform.childCount;
+        int alreadyHave = objectsToSpreadString.Count;
+        print("alreadyHave " + alreadyHave);
+        for (int j = alreadyHave; j < positionsCount; j++)
+        {
+            objectsToSpreadString.Add("Stop");
+        }
+
+        print(objectsToSpreadString.Count + " " + positionsCount);
+
+        for (int i = 0; i < positionsCount; i++)
+        {
+            int index = Random.Range(0, objectsToSpreadString.Count);
+            print(index);
+            GameObject itemToSpreadLoad = (GameObject) Resources.Load(objectsToSpreadString[index], typeof(GameObject));
+            Instantiate(itemToSpreadLoad, transform.GetChild(i).position, Quaternion.identity);
+            objectsToSpreadString.RemoveAt(index);
+        }
+
+        print(objectsToSpreadString.Count + " " + positionsCount);
     }
 }
